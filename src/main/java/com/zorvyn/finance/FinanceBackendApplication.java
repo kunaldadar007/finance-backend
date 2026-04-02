74d59669dc48c416c3196bfb16e7058be494973d
Kunal Dadar <kunaldadar007@gmail.com>
2026-04-02 12:54:02 +0530

    fix: update service and config

--- src/main/java/com/zorvyn/finance/FinanceBackendApplication.java
+++ src/main/java/com/zorvyn/finance/FinanceBackendApplication.java
@@ -2,8 +2,10 @@ package com.zorvyn.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zorvyn")
+@EnableJpaRepositories(basePackages = "com.zorvyn.finance.repository")
 public class FinanceBackendApplication {
 
 	public static void main(String[] args) {
